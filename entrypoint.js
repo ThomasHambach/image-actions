b9a3560aba0471ccd60030c3bb5e2a3011d8b665
Thomas Hambach <t.hambach@gmail.com>
2020-09-09 16:43:18 +0800

    Update entrypoint.js

--- entrypoint.js
+++ entrypoint.js
@@ -11,23 +11,6 @@ if (!GITHUB_TOKEN) {
 }
 
 const main = async () => {
-  // Bail out if the event that executed the action wasn’t a pull_request
-  if (GITHUB_EVENT_NAME !== "pull_request") {
-    console.log("::error:: This action only runs for pushes to PRs");
-    process.exit(78);
-  }
-
-  // Bail out if the pull_request event wasn't synchronize or opened
-  const event = await githubEvent();
-  if (event.action !== "synchronize" && event.action !== "opened") {
-    console.log(
-      "::error:: Check run has action",
-      event.action,
-      ". Wants: synchronize or opened"
-    );
-    process.exit(78);
-  }
-
   await run();
 };
 
